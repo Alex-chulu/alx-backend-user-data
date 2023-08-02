37640423e8c6f075420cc8068667280bb9c7fd3d
Alex-chulu <chulualexander@gmail.com>
2023-08-02 20:24:18 +0000

    task 0 fix

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
+"""
+Main file
+"""
+
 
 import re
+
 
 def filter_datum(fields, redaction, message, separator):
     return re.sub(r'({})'.format('|'.join(fields)), redaction, message)
