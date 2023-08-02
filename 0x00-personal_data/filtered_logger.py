dba5df494f8da1256395519026dee1a5445cb71d
Alex-chulu <chulualexander@gmail.com>
2023-08-02 20:09:54 +0000

    Regex-ing

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+import re
+
+def filter_datum(fields, redaction, message, separator):
+    return re.sub(r'(?<=\{}{})(.*?)(?=\{}|$)'.format(separator, 
+        '|'.join(fields), separator), redaction, message)
